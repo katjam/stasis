cb0b16658cdc09774e52a34acd25811cc28b93b9
Dillon Kearns <dillon@dillonkearns.com>
2019-06-29 13:38:24 +0200

    Remove unneeded boilerplate.

--- src/Main.elm
+++ src/Main.elm
@@ -27,43 +27,22 @@ main =
         }
 
 
-
--- MODEL
-
-
-type Model
-    = Failure
-    | Loading
-    | Success String
+type alias Model =
+    ()
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Loading, getRandomCatGif )
+    ( (), Cmd.none )
 
 
-
--- UPDATE
-
-
-type Msg
-    = MorePlease
-    | GotGif (Result Http.Error String)
+type alias Msg =
+    ()
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        MorePlease ->
-            ( Loading, getRandomCatGif )
-
-        GotGif result ->
-            case result of
-                Ok url ->
-                    ( Success url, Cmd.none )
-
-                Err _ ->
-                    ( Failure, Cmd.none )
+    ( (), Cmd.none )
 
 
 
@@ -87,39 +66,3 @@ viewDocument model =
 view : Model -> Html Msg
 view model =
     World.init |> World.view
-
-
-viewGif : Model -> Html Msg
-viewGif model =
-    case model of
-        Failure ->
-            div []
-                [ text "I could not load a random cat for some reason. "
-                , button [ onClick MorePlease ] [ text "Try Again!" ]
-                ]
-
-        Loading ->
-            text "Loading..."
-
-        Success url ->
-            div []
-                [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
-                , img [ src url ] []
-                ]
-
-
-
--- HTTP
-
-
-getRandomCatGif : Cmd Msg
-getRandomCatGif =
-    Http.get
-        { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
-        , expect = Http.expectJson GotGif gifDecoder
-        }
-
-
-gifDecoder : Decoder String
-gifDecoder =
-    field "data" (field "image_url" string)
